30152b4001e7f53694349bd886de99d63161a2c9
Vyachesla Lee <2nxh67pkzt-blip>
2026-04-30 17:08:10 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const yesBtn = document.getElementById('yes-button');
+const yesBtn2 = document.getElementById('yes-button2');
+const yesBtn3 = document.getElementById ('yes-button3')
+const noBtn = document.getElementById('no-button');
+
+if (noBtn) {
+  noBtn.addEventListener('click', () => {
+    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+  });
+}
+
+if (yesBtn) {
+  yesBtn.addEventListener('click', () => {
+    location.href = '../html/page2.html';
+  });
+}
+
+if (yesBtn2) {
+  yesBtn2.addEventListener('click', () => {
+    location.href = '../html/page3.html';
+  });
+}
+
+if (yesBtn3) {
+  yesBtn3.addEventListener('click', () => {
+    location.href = '../html/lastPage.html';
+  });
+}
